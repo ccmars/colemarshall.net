a2fc6181288925d4656d8134577f5302f22e718b
Cole <cm@colemarshall.net>
2017-06-05 23:55:53 -0500

    Add icons to code samples

--- includes/codeSampleItems.php
+++ includes/codeSampleItems.php
@@ -5,63 +5,74 @@ $codeSampleItems = [
 		'name' => 'HTML',
 		'description' => 'Hypertext Markup Language, defines the structure of a web page',
 		'file' => 'html.html',
-		'fiddle' => 'https://jsfiddle.net/CCMars/sct5L8pu/'
+		'fiddle' => 'https://jsfiddle.net/CCMars/sct5L8pu/',
+		'icon' => 'devicon-html5-plain'
 	],
 	'css' => [
 		'name' => 'CSS',
 		'description' => 'Cascading Style Sheet, defines the style of the web page',
 		'file' => 'css.css',
-		'fiddle' => 'https://jsfiddle.net/CCMars/k27nrqd5/'
+		'fiddle' => 'https://jsfiddle.net/CCMars/k27nrqd5/',
+		'icon' => 'devicon-css3-plain'
 	],
 	'javascript' => [
 		'name' => 'JavaScript',
 		'description' => 'Makes the web page interactive in the foreground',
 		'file' => 'javascript.js',
-		'fiddle' => 'https://jsfiddle.net/CCMars/ur1t5Ld5/'
+		'fiddle' => 'https://jsfiddle.net/CCMars/ur1t5Ld5/',
+		'icon' => 'devicon-javascript-plain'
 	],
 	'php' => [
 		'name' => 'PHP',
 		'description' => 'PHP Hypertext Preprocessor, makes the web page interactive in the background',
 		'file' => 'php.php',
-		'fiddle' => 'http://ideone.com/cVtpIL'
+		'fiddle' => 'http://ideone.com/cVtpIL',
+		'icon' => 'devicon-php-plain'
 	],
 	'mysql' => [
 		'name' => 'MySQL',
-		'description' => 'Stores data for the webpage',
+		'description' => 'Stores data for the web page',
 		'file' => 'sql.sql',
-		'fiddle' => 'http://sqlfiddle.com/#!9/ff8c4'
+		'fiddle' => 'http://sqlfiddle.com/#!9/ff8c4',
+		'icon' => 'devicon-mysql-plain'
 	],
 	'scss' => [
 		'name' => 'Sass',
 		'description' => 'Syntactically Awesome Style Sheet, makes defining the style of the web page easier',
 		'file' => 'scss.scss',
-		'fiddle' => 'https://jsfiddle.net/CCMars/cy91n613/'
+		'fiddle' => 'https://jsfiddle.net/CCMars/cy91n613/',
+		'icon' => 'devicon-sass-original'
 	],
 	// 'jquery' => [
 	// 	'name' => 'jQuery',
 	// 	'description' => 'Makes creating the interactive web page easier',
-	// 	'file' => 'jquery.js'
+	// 	'file' => 'jquery.js',
+	// 	'icon' => 'devicon-jquery-plain'
 	// ],
 	'http' => [
 		'name' => 'REST',
 		'description' => 'Representational state transfer, makes transfering data between applications easier',
-		'file' => 'rest.txt'
+		'file' => 'rest.txt',
+		'icon' => 'fa fa-server'
 	],
 	'xml' => [
 		'name' => 'XML',
-		'description' => 'Extensible Markup Language, a format for storing data for the webpage or application',
-		'file' => 'xml.xml'
+		'description' => 'Extensible Markup Language, a format for storing data for the web page or application',
+		'file' => 'xml.xml',
+		'icon' => 'fa fa-code'
 	],
 	'json' => [
 		'name' => 'JSON',
-		'description' => 'JavaScript Object Notation, a format for storing data for the webpage or application',
-		'file' => 'json.json'
+		'description' => 'JavaScript Object Notation, a format for storing data for the web page or application',
+		'file' => 'json.json',
+		'icon' => 'fa fa-database'
 	],
 	'git' => [
 		'name' => 'Git',
-		'description' => 'Allows the tracking of changes to a web page',
+		'description' => 'Allows the tracking of changes to a web page or application',
 		'file' => 'git.txt',
-		'interpret' => 'hljs nohighlight'
+		'interpret' => 'hljs nohighlight',
+		'icon' => 'devicon-git-plain'
 	]
 ];
 ?>
