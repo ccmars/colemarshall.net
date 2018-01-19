a8f7e773c272b469942115608544c633c1dd0322
Cole <cm@colemarshall.net>
2018-01-18 21:51:42 -0600

    ES6 JavaScript example

--- data/code/javascript.js
+++ data/code/javascript.js
@@ -1,36 +1,38 @@
 // Cole Class
-var Cole = function(initialProfession) {
-	this.nameFirst = "Cole";
-	this.nameLast = "Marshall";
-	this.profession = initialProfession;
-	this.skills = [
-		"Frontend Web Development",
-		"Backend Web Development",
-		"Web Design"
-	];
-}
-
-Cole.prototype.getFullName = function() {
-	return this.nameFirst + ' ' + this.nameLast;
-};
-
-Cole.prototype.getReadableDetails = function() {
-	var readable = this.getFullName() + " is " + (this.profession.match(/^[aeiou]/i)?"an ":"a ") + this.profession + " who specializes in ";
-	for (var i = 0; i < this.skills.length; i++) {
-		readable += this.skills[i];
-		if (i == this.skills.length - 2) {
-			readable += (this.skills.length > 2?",":'') + " and ";
-		} else if (i == this.skills.length - 1) {
-			readable += ".";
-		} else {
-			readable += ", ";
-		}
+class Cole {
+	constructor(initialProfession) {
+		this.nameFirst = "Cole";
+		this.nameLast = "Marshall";
+		this.profession = initialProfession;
+		this.skills = [
+			"Frontend Web Development",
+			"Backend Web Development",
+			"Web Design"
+		];
 	}
-	return readable;
+
+	getFullName() {
+		return this.nameFirst + ' ' + this.nameLast;
+	}
+
+	getReadableDetails() {
+		let readable = this.getFullName() + " is " + (this.profession.match(/^[aeiou]/i)?"an ":"a ") + this.profession + " who specializes in ";
+		for (let i = 0; i < this.skills.length; i++) {
+			readable += this.skills[i];
+			if (i === this.skills.length - 2) {
+				readable += (this.skills.length > 2?",":'') + " and ";
+			} else if (i === this.skills.length - 1) {
+				readable += ".";
+			} else {
+				readable += ", ";
+			}
+		}
+		return readable;
+	}
 }
 
 // Instantiate Cole
-var cole = new Cole("Interactive Developer & Designer");
+let cole = new Cole("Interactive Developer & Designer");
 
 // Echo Cole's Profession and Skills
 console.log(cole.getReadableDetails());
