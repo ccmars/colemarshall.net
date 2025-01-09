a659f224e329ba23b0d70b1d3f19317c8f5dabde
Cole Marshall <cole@colemarshall.net>
2025-01-08 22:59:49 -0600

    Remove jQuery from home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-	$('code').click(function() {
-		$(this).toggleClass('expanded');
-	});
+document.addEventListener('DOMContentLoaded', function() {
+	// Expand and collapse code snippets
+  document.querySelectorAll('code').forEach(function(codeElement) {
+    codeElement.addEventListener('click', function() {
+      this.classList.toggle('expanded');
+    });
+  });
 
-	console.log("%cLook at you! You seem to know where the really good stuff is. I like that.","color:#C0D3B1; background-color:#3B3C36; font-size:180%; padding:8px; font-weight:bold; border-radius:12px;");
-
-	$('.svgImg').svgInject();
+  console.log(
+    "%cLook at you! You seem to know where the really good stuff is. I like that.",
+    "color:#C0D3B1; background-color:#3B3C36; font-size:180%; padding:8px; font-weight:bold; border-radius:12px;"
+  );
 });
